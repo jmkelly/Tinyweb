41de4508f88bfaac4ac970b87c4490461b07d62f
James Kelly <james.kelly.99@gmail.com>
2011-07-11 17:25:43 +1000

    Initial partial attempt at a select_tag helper

---
+++ src/tinyweb.framework.tests/Helpers/SeletTagTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using tinyweb.framework.Helpers;
+using Moq;
+using System.Web;
+using System.Web.Routing;
+
+namespace tinyweb.framework.tests
+{
+    [TestFixture] 
+    public class SeletTagTests
+    {
+
+
+        //IEnumerable<HandlerData> handlers;
+
+    
+        [SetUp]
+        public void Setup()
+        {
+          
+
+            Tinyweb.Handlers = new List<HandlerData>
+            {
+                new HandlerData {Type = typeof(SampleModelHandler) }
+            };
+
+            
+        }
+
+       
+
+        [Test]
+        public void SelectTag_returns_select_html()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<select>");
+
+        }
+
+
+                
+    }
+
+    public class SampleModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class SampleModelHandler
+    {
+        public IResult Get()
+        {
+
+            return new JsonResult(createDummySampleModels());
+        }
+
+        private List<SampleModel> createDummySampleModels()
+        {
+            List<SampleModel> sampleModels = new List<SampleModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                SampleModel sm = new SampleModel { id = i, name = "name" + i.ToString() };
+                sampleModels.Add(sm);
+            }
+            return sampleModels;
+
+        }
+
+       
+    }
+
+}
---
+++ src/tinyweb.framework/Helpers/SelectTag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Routing;
+
+namespace tinyweb.framework.Helpers
+{
+    public static class SelectTag
+    {
+
+               
+
+        public static string For<T>(string value, string text)
+        {
+
+            var handler = Tinyweb.Handlers.SingleOrDefault(h => h.Type == typeof(T));
+            if (handler == null)
+            {
+                throw new Exception("The handler {0} was not found".With(typeof(T).Name));
+            }
+
+           
+            IHandlerInvoker hi = HandlerInvoker.Current;
+            return string.Empty;
+            //ExecutionResult er = hi.Execute(handler, this._requestContext, null);
+            //return er.Result.ToString();            
+            
+         }
+
+        public static string BuildOption(string value, string text)
+        {
+            return "<option value='" + value + "'>" + text + "</option>";
+        }
+
+    }
+}
